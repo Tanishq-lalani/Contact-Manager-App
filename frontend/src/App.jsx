167b149695db038969db4ef92878054dddfba51f
Tanishq lalani <tanishqjain421@gmail.com>
2025-09-08 22:38:03 +0530

    Modified App file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Navbar from './components/Navbar'
 function App() {
   const [count, setCount] = useState(0)
+  const token = localStorage.getItem("token")
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello World!</h1>
+      <Routes>
+       {!token ? (<>
+          <Route path='/login' element={<Login/>}/>
+       </>) : 
+      (
+        <>
+         <Navbar/>
+           
+          
+         <Footer/>
+        </>
+      )}
+
+      </Routes>
     </>
   )
 }

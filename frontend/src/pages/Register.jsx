a5ea43a752cf4a1d6612220b7be83978ac25cfa8
Tanishq lalani <tanishqjain421@gmail.com>
2025-09-09 20:20:28 +0530

    Login and Register Page Modified

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -33,7 +33,6 @@ const Register = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="w-full max-w-md bg-white rounded-2xl shadow-lg p-8">
         <form onSubmit={handleRegister} className="space-y-6">
-          {/* ✅ Inline text + link */}
           <div className="flex justify-center space-x-2 text-sm text-gray-600">
             <span>Already Register?</span>
             <button
